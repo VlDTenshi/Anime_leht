14f2fa25eff0f4101e39ccf44d855c4436d29fa9
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-12-05 14:41:30 +0200

    As an app developer I want to get a list of Anime titles in Estonian language to show in my app [Delivers #186567028]

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const port = 8080;
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/Animes', (req, res)=>{
+    res.send(["Kiseiju Seino Kakurisu", "Kimi no todoke", "Akame ga Kilu", "Death note"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.listen(port, () => {console.log(`API up at: http://localhost:${port}`)})
