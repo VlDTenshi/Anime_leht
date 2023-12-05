beb67fcfd5e4cba3375540759ce58cb6be1e4797
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-12-05 16:45:39 +0200

    As an app developer I want to get full details of any given Anime title in Estonian language to show up in my app[Delivers #186567094]

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const app = require('express')();
 const port = 8080;
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs');
+//const swaggerDocument = require('./docs/swagger.json');
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 
-app.get('/Animes', (req, res)=>{
-    res.send(["Kiseiju Seino Kakurisu", "Kimi no todoke", "Akame ga Kilu", "Death note"])
+const animes =[
+    {id:1, name:"Parasyte: Teaching about life", Ilmumiseaasta:2014, rating: 8.8},
+    {id:2, name:"Reach you", Ilmumiseaasta:2009, rating: 7.8},
+    {id:3, name:"Akame ga Kill", Ilmumiseaasta:2014, rating: 7.7},
+    {id:4, name:"Death note", Ilmumiseaasta:2006, rating: 8.6},
+    {id:5, name:"Elf song", Ilmumiseaasta:2004, rating: 7.9}
+]
+
+app.get('/animes', (req, res)=>{
+    res.send(animes)
+})
+
+app.get("/animes/:id", (req, res)=>{
+    if(typeof animes[req.params.id - 1] === 'undefined')
+    {
+        return res.status(404).send({error: "Anime ei ole leitud"})
+    }
+    res.send(animes[req.params.id - 1])
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
